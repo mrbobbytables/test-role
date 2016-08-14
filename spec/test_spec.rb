bf71557bfeebcac41ffb5b47f77202b5c32339e9
Bob Killen <rkillen@umich.edu>
2016-08-14 09:43:23 -0400

    added failure exit code override

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -11,14 +11,12 @@ context ENV['KITCHEN_INSTANCE'] || host_inventory['hostname'] do
       it { should be_mode 644 }
     end
 
-    if os[:family] == 'ubuntu'
       describe file('/tmp/conditional') do
         it { should exist }
         it { should be_owned_by 'vagrant' }
         it { should be_grouped_into 'vagrant' }
         it { should be_mode 644 }
         its(:content) { should match(property['playbook_var']) }
-      end
     end
   end
 end
