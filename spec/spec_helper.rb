d76ad9af31dc369b728b40cfa59f2838d993d963
Bob Killen <rkillen@umich.edu>
2016-08-16 13:53:19 -0400

    updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,11 +31,13 @@ set :env, LANG: 'C', LC_ALL: 'C'
 
 RSpec.configure do |c|
   c.failure_exit_code = ENV['RSPEC_FAILURE_EXIT_CODE'] || 1
-  if ENV['RSPEC_FORMAT'] == 'junit'
-    c.output_stream = File.open("#{ENV['KITCHEN_INSTANCE']}.junit.xml", 'w')
-    c.formatter = 'JUnit'
-  else
-    c.formatter = 'documentation'
+  if ENV['RSPEC_FORMATTER']
+    if ENV['RSPEC_FORMATTER'].casecmp('junit').zero?
+      c.output_stream = File.open("#{ENV['KITCHEN_INSTANCE']}.junit.xml", 'w')
+      c.add_formatter('JUnit')
+    else
+      c.add_formatter(ENV['RSPEC_FORMATTER'])
+    end
   end
 end
 
